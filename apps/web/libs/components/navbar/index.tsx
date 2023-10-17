aa7de5901b629abf80959362621a9492037533f4
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-10-17 16:04:37 +0330

    fix the navbar

--- apps/web/libs/components/navbar/index.tsx
+++ apps/web/libs/components/navbar/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+// TODO ما الان چون میخوایم ادرس فعلی رو داشته باشیم مجبور شدیم از یوز کلاینت استفاده کنیم . ولی ایا راهی هست که ادرس فعلی رو سمت سرور دریافتش کنیم و کامپوننت مون سرور کامپوننت باشه ؟
 import {
   ABOUT_ME_ROUTE,
   BLOG_ROUTE,
@@ -7,50 +9,39 @@ import {
 } from '../../constants';
 import Link from 'next/link';
 import React from 'react';
+import { cn } from '../../utils';
+import { usePathname } from 'next/navigation';
+
+const navItems = [
+  { href: ABOUT_ME_ROUTE, text: 'درباره من' },
+  { href: RESUME_ROUTE, text: 'رزومه' },
+  { href: PROJECTS_ROUTE, text: 'نمونه کار' },
+  { href: BLOG_ROUTE, text: 'بلاگ' },
+  { href: CONtACT_ME_ROUTE, text: 'ارتباط با من' },
+];
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link href={ABOUT_ME_ROUTE}>
-            <button className="navbar-link active" data-nav-link>
-              درباره من
-            </button>
-          </Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link href={RESUME_ROUTE}>
-            <button className="navbar-link" data-nav-link>
-              رزومه
-            </button>
-          </Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link href={PROJECTS_ROUTE}>
-            <button className="navbar-link" data-nav-link>
-              نمونه کار
-            </button>
-          </Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link href={BLOG_ROUTE}>
-            <button className="navbar-link" data-nav-link>
-              بلاگ
-            </button>
-          </Link>
-        </li>
-
-        <li className="navbar-item">
-          <Link href={CONtACT_ME_ROUTE}>
-            <button className="navbar-link" data-nav-link>
-              ارتباط با من
-            </button>
-          </Link>
-        </li>
+        {navItems.map((item) => (
+          <li key={item.href} className="navbar-item">
+            <Link href={item.href}>
+              <button
+                className={cn(
+                  'navbar-link',
+                  pathname === item.href ? 'active' : ''
+                )}
+                data-nav-link
+              >
+                {item.text}
+              </button>
+            </Link>
+          </li>
+        ))}
+      
       </ul>
     </nav>
   );
