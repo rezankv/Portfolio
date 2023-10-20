15a089304e8c6deaa134d8f428e57ce9aeb366ed
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-10-20 13:22:18 +0330

    test change

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -5,7 +5,7 @@ import { ABOUT_ME_ROUTE } from '../libs/constants';
 const Page = () => {
   return (
     <article className="notfound-section ">
-      <h1 className="notfound-title">404</h1>
+      <h1 className="notfound-title">۴۰۴</h1>
       <div className="notfound-description">محتوا مورد نظر یافت نشد</div>
       <Link
         target="_parent"
