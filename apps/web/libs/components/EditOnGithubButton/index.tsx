f5b40c9af8e088eb43ef15c7ecfdedf9964907fe
Reza Nikravesh <36360043+rezankv@users.noreply.github.com>
2023-11-10 12:58:33 +0330

    create the edit button (#26)

---
+++ apps/web/libs/components/EditOnGithubButton/index.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+  href: string;
+}
+const EditOnGithubButton = ({ href }: Props) => {
+  return (
+    <Link
+      href={href}
+      style={{
+        display: 'flex',
+        gap: '.5rem',
+        marginTop: '4rem',
+        color: 'var(--vegas-gold)',
+      }}
+      target="_blank"
+    >
+      <img
+        style={{ width: '20px', marginBlock: '0' }}
+        src={'/images/icon-github.svg'}
+        alt={'github-icon'}
+      />
+      ویرایش در گیتهاب
+    </Link>
+  );
+};
+
+export default EditOnGithubButton;
