7043ede5d19841023dd9130f5a4fca4b8df6a623
Reza Nikravesh <36360043+rezankv@users.noreply.github.com>
2024-08-02 12:52:23 +0330

    develop to production (#55)
    * Revert "define cache time in next config file (#36)"
    
    This reverts commit 58928366f2b5b17445565b36433fb46bf00d39fc.
    
    * Revert "Revert "define cache time in next config file (#36)""
    
    This reverts commit 9faa8f01d221997694ae470dd5c6b9bb6744875b.
    
    * update next config
    
    * update TODO comments (#39)
    
    * replace img tag with next image (#40)
    
    * fix service margin (#41)
    
    * Feature/config import alias and update imports (#42)
    
    * config import alias
    
    * update imports
    
    * Update README.md (#44)
    
    * chore: update work experience (#45)
    
    * Update social links (#47)
    
    * add wrangler.toml file (#49)
    
    * Update wrangler.toml (#50)
    
    * Update wrangler.toml (#51)
    
    * Update wrangler.toml (#52)
    
    * Update wrangler.toml (#53)
    
    * update location (#54)

--- apps/web/app/(main)/contact-me/page.tsx
+++ apps/web/app/(main)/contact-me/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
         <section className="mapbox" data-mapbox>
           <figure>
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218010.7427231114!2d48.505780786927424!3d31.375039933167137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fc3df8b8d29bbf5%3A0x4968f1dd5789625c!2sAhvaz%2C%20Khuzestan%20Province%2C%20Iran!5e0!3m2!1sen!2s!4v1697559247434!5m2!1sen!2s"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53902.01279234507!2d48.39738739999999!3d32.39569089999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fe9c0026ffde9d7%3A0x5f778849aa5c2962!2sDezful%2C%20Khuzestan%20Province!5e0!3m2!1sen!2s!4v1722590095945!5m2!1sen!2s"
               width="400"
               height="300"
               loading="lazy"
